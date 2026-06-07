547c1df5a1e9a95e25fb273f3eebad9971ff5e5d
rite2harini <rite2harini@gmail.com>
2026-06-07 13:15:09 +0530

    Add BankApplication.java with project requirements
    Initial setup for the banking system application with requirements and concepts outlined.

---
+++ 05_OOP_Banking_System/BankApplication.java
@@ -0,0 +1,15 @@
+/*
+STAGE 5 - USING OBJECTS
+
+REQUIREMENTS
+
+1. Create multiple Account objects.
+2. Perform transactions.
+3. Display account information.
+
+CONCEPTS ALLOWED
+
+Classes
+Objects
+Methods
+*/
