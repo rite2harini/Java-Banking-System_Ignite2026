bb8a58ce6b588ffc8c841c39448a447cc47dc627
rite2harini <rite2harini@gmail.com>
2026-06-07 13:42:49 +0530

    Add Account class skeleton with comments
    Created Account class with attributes and methods outlined in comments.

---
+++ src/model/Account.java
@@ -0,0 +1,21 @@
+package model;
+
+/*
+IMPLEMENT DURING OOP MODULE
+
+ATTRIBUTES
+
+accountNumber
+customerName
+balance
+
+METHODS
+
+deposit()
+withdraw()
+displayAccount()
+*/
+
+public class Account {
+
+}
