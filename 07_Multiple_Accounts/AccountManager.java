030b0b25ff5b665c7cbcaf9b729b241dab1df13c
rite2harini <rite2harini@gmail.com>
2026-06-07 13:17:50 +0530

    Create AccountManager for multiple account handling
    Implement AccountManager class for managing multiple accounts.

---
+++ 07_Multiple_Accounts/AccountManager.java
@@ -0,0 +1,19 @@
+/*
+STAGE 7
+
+REQUIREMENTS
+
+Store multiple accounts using array.
+
+FEATURES
+
+Search by account number.
+
+Search by customer name.
+
+Display all accounts.
+
+BONUS
+
+Money Transfer.
+*/
