9ebd62da07974eff4dd7969007f6dbc41e83681f
rite2harini <rite2harini@gmail.com>
2026-06-07 13:42:04 +0530

    Create BankApplication.java

---
+++ src/app/BankApplication.java
@@ -0,0 +1,42 @@
+package app;
+
+/*
+JAVA BANKING SYSTEM
+
+STARTING STAGE
+
+Topics Covered:
+Variables
+Scanner
+Conditions
+Loops
+Methods
+OOP
+Arrays
+Exception Handling
+
+Students will continuously upgrade this project.
+
+CURRENT TASK
+
+1. Create Account
+2. Take User Input
+3. Display Details
+
+Future Features
+
+Deposit
+Withdraw
+Transfer
+PIN Verification
+ATM Simulation
+*/
+
+public class BankApplication {
+
+    public static void main(String[] args) {
+
+        // TODO
+
+    }
+}
