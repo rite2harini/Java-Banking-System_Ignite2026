155d45af21e58d6102cd3e999f1e224d8f5a1aaf
rite2harini <rite2harini@gmail.com>
2026-06-07 13:31:07 +0530

    Update Money_Transfer_System.java

--- Bonus_Challenges/Money_Transfer_System.java
+++ Bonus_Challenges/Money_Transfer_System.java
@@ -1,1 +1,18 @@
+/*
+ADVANCED CHALLENGE
 
+Requirements:
+
+Create two accounts.
+
+Transfer money from one account to another.
+
+Rules:
+
+Transfer amount must be positive.
+
+Sender must have sufficient balance.
+
+Bonus:
+Display transaction details.
+*/
