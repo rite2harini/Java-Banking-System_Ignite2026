6bf791e0fd901a884c89928db638a0d2f6ca2866
rite2harini <rite2harini@gmail.com>
2026-06-07 13:32:03 +0530

    Add comments for advanced challenge on transaction history
    Added comments outlining the advanced challenge requirements for maintaining transaction history and displaying a complete statement.

--- Bonus_Challenges/Mini_Statement.java
+++ Bonus_Challenges/Mini_Statement.java
@@ -1,1 +1,18 @@
+/*
+ADVANCED CHALLENGE
 
+Maintain transaction history.
+
+Example:
+
+Deposited Rs.5000
+Withdrawn Rs.1000
+Deposited Rs.2000
+
+Display complete statement.
+
+Hint:
+
+Use String variables for now.
+Collections will be used later.
+*/
