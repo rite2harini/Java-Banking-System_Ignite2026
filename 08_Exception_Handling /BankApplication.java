2890ddb15c12933fbd543d99eb01a9b0a77fe0e2
rite2harini <rite2harini@gmail.com>
2026-06-07 13:23:31 +0530

    Document exception handling in BankApplication
    Add comments for exception handling scenarios and usage.

--- 08_Exception_Handling /BankApplication.java
+++ 08_Exception_Handling /BankApplication.java
@@ -1,1 +1,26 @@
 
+/*
+STAGE 8
+
+HANDLE
+
+Negative Deposit
+
+Invalid Withdrawal
+
+Invalid Menu Choice
+
+Incorrect PIN
+
+USE
+
+try
+catch
+finally
+throw
+throws
+
+BONUS
+
+Account lock after 3 incorrect attempts.
+*/
