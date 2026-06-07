01cdc766760280d175577ef1d18c3870bfb0313c
rite2harini <rite2harini@gmail.com>
2026-06-07 13:14:28 +0530

    Create Account class with attributes and methods
    Define the Account class with attributes and methods.

---
+++ 05_OOP_Banking_System/Account.java
@@ -0,0 +1,24 @@
+/*
+STAGE 5 - ACCOUNT CLASS
+
+CREATE
+
+Class Name:
+Account
+
+ATTRIBUTES
+
+accountNumber
+customerName
+balance
+
+METHODS
+
+deposit()
+withdraw()
+displayAccount()
+
+BONUS
+
+Add accountType.
+*/
