a805423da7611177002d9969fbe2f6676dad12f9
rite2harini <rite2harini@gmail.com>
2026-06-07 18:06:41 +0530

    Create ATMChallenge class for ATM simulation
    Initial implementation of ATM simulation class.

---
+++ src/challenge/ATMChallenge.java
@@ -0,0 +1,23 @@
+package challenge;
+
+/*
+BONUS CHALLENGE
+
+Create ATM Simulation
+
+Features
+
+PIN Verification
+Deposit
+Withdraw
+Balance Check
+Exit
+
+Bonus
+
+Lock account after 3 invalid attempts.
+*/
+
+public class ATMChallenge {
+
+}
